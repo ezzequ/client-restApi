ee247fbbb151247c981ff0c90b6f4565cb915b4f
ezzequ <esther.reichmann@me.com>
2022-05-06 18:31:47 +0200

    updates

--- index.js
+++ index.js
@@ -1,81 +1,89 @@
-
-const express = require("express");
-const app = express();
-const fs = require("fs");
+import express from "express"
+import fs from "fs";
+import cros from "cors"
+import bodyParser from "body-parser"
 const hostname = "localhost";
 const port = 5500;
+const app = express();
 
+app.use("/", express.static("public"));
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use("/", (req, res, next) => {
   console.log("api visited");
   next();
 });
 
-app.use("/", express.static("public"));
-
-let mangas = [
-  {
-    name: "Attack On Titan",
-    genre: "action",
-    chapters: "149",
-    id: "a",
-  },
-  {
-    name: "Tokyo Ghoul",
-    genre: "dark fantasy",
-    chapters: "143",
-    id: "b",
-  },
-  {
-    name: "Demon Slayer",
-    genre: "dark fantasy",
-    chapters: "250",
-    id: "c",
+const getMangasFS = () => {
+   let mangaFromJson = fs.readFileSync("./mangas.json")
+   let mangasParse = JSON.parse(mangaFromJson);
+   return mangasParse
   }
-  // {
-  //   name: "death Note",
-  //   genre: "thriller",
-  //   chapters: "108",
-  //   id: "d",
-  // },
-  // {
-  //   name: "Fullmetal Alchemist",
-  //   genre: "adventure",
-  //   chapters: "116",
-  //   id: "e",
-  // }
-];
+  
 
-app.get("/api/mangas", (req, res) => {
-  res.send(mangas);
+
+
+const mangasWrite = (data) =>{
+  fs.writeFile("./mangas.json",JSON.stringify(data),(err)=>{
+    if (err){
+      console.error(err)
+    }
+    });
+}
+
+function randomID() {
+  let ID = Math.random(0.4) * 100;
+  
+  return ID;
+}
+
+
+
+let mangas = getMangasFS();
+app.get("/api/mangas", (req, res, next) => {
+  res.send(getMangasFS());
+  next()
 });
 
-app.post("/api/mangas", (req, res) => {
-  const manga = req.body
-  res.status(201);
-  console.log(req.body);
-  mangas.push({...manga});
-  res.send("your manga has been added to the list");
+app.post("/api/mangas", (req, res, next) => {
+  const addManga = req.body
+  mangas.push({...addManga, id:randomID()});
+
+  mangasWrite(mangas)
+  res.status(200).send("your manga has been added to the list");
+  next()
 });
 
+app.put("/api/mangas/:id",(req,res, next) => {
+let { id } = req.params;
+let mangaToEdit = mangas.find((manga) => manga.id == id);
+if (!mangaToEdit) {
+  res.status(404).send("could not find manga");
 
-app.delete("/api/mangas/:id",(req, res) => {
+  }
+let updatedManga = mangas.map((manga) => {
+    if (manga.id == id){
+      return req.body
+    }
+    return manga
+  })
+  mangasWrite(updatedManga)
+  res.status(200).send("manga Updated")
+  next()
+});
+
+app.delete("/api/mangas/:id",(req, res, next) => {
   let { id } = req.params;
-  let mangaToDelete = mangas.filter((manga) => manga.id !== id);
-  console.log(mangaToDelete)
-  //  if (mangaToDelete == -1) {
-  //   // const updatedMangas = mangas.filter(test => test.id != id);
-  //   //     mangas = updatedMangas
-      
-  //   let updatedMangas = mangas.splice(mangaToDelete,1);
-  //   updatedMangas = mangas
-  //   console.log(updatedMangas);
-    
-  // }
-  // else if (mangaToDelete != -1) {
-  //   res.status(404).send("something wrong");
-  // }
+  let mangaToDelete = mangas.find((manga)=> manga.id == id);
+  if (!mangaToDelete){
+    res.status(404).send("could not find manga")
+  }
+  let mangaNewAfterDeteted = mangas.filter((manga) => manga.id != id);
+  mangasWrite(mangaNewAfterDeteted)
+  res.status(200).send("manga deleted")
+  next()
   
 });
 
@@ -83,4 +91,9 @@ app.delete("/api/mangas/:id",(req, res) => {
 app.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}`);
 });
+
+
+
+
+
 
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 ndividuellt skapa ett enklare REST-API i NodeJS med express. Då API:et skall har funktionalitet för CRUD skall det finnas 4 st endpoints inkluderat GET, DELETE, PUT och POST för en resurs. Varje endpoint skall utföra vad dess anropsmetod ämnar att utföra, dvs. POST för att lägga till, PUT för att uppdatera, GET för att hämta osv. GET anropet skall hämta alla objekt. Vad API:et hanterar för typ av resurs väljer du själva, det viktiga är att det finns minst 4 st egenskaper för ett objekt varav en egenskap är id. Projektet skall inte ha någon databas utan istället ha all data sparad lokalt i serverfilen (exempelvis en array med produkter). För att testa ert API rekommenderas du att använda VS Code tilläget REST Client.
+
+1. before starting the app write "npm i" in your terminal to download all dependencies. 
+
+2. write "npm start" to start the project!
+the port its connected to will get console logged in your terminal, you can press the link to start your server. 
